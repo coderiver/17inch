2a8c48dbf17087487fa4300ea7e96c06728157ba
Splintex <godun.yevgen@gmail.com>
2013-12-23 11:29:43 +0200

    filter dropdown

--- skin/frontend/17inches/default/js/common.js
+++ skin/frontend/17inches/default/js/common.js
@@ -40,5 +40,47 @@ $j(document).ready(function(){
       $j('.filter').removeClass('is-fixed');
     }
   });
+
+
+      $j(document).click(function() {
+          $j(".js-select-list").hide();
+          $j(".js-select").removeClass("is-active");
+      });
+      function select() {
+          $j(".js-select").each(function(){
+              var select_list = $j(this).find(".js-select-list");
+              var text = select_list.find("li").first().text();
+              $j(this).find(".js-select-text").text(text);
+              $j(this).click(function(event){
+                  if ($j(this).hasClass("is-active")) {
+                      $j(this).removeClass("is-active");
+                      select_list.slideUp("fast");
+                  }
+                  else {
+                      $j(".js-select").removeClass("is-active");
+                      $j(".js-select-list").hide();
+                      select_list.slideDown("fast");
+                      $j(this).addClass("is-active");
+                  }
+                  event.stopPropagation();
+              });
+              select_list.find("li").click(function(event) {
+                  var id = $j(this).attr("data-id");
+                  var text = $j(this).text();
+                  $j(this).parent().parent().find(".js-select-text").text(text);
+                  $j(this).parent().parent().find(".js-select-input").val(id);
+                  $j(this).parent().hide();
+                  $j(this).parents(".js-select").removeClass("is-active");
+                  event.stopPropagation();
+              });
+          });
+      }
+      
+      select();
+      $j('.js-select').click(function(event){
+          event.stopPropagation();
+      })
+
+
 });		
 
